6a0ee7d9b3fe9ba9b5bd062ad31abab0bebe41d9
Niyireth Cortes <niyirethcortes@periferia-it.com>
2022-03-09 08:28:20 -0500

    Agregación de dos puntos.

--- Pagos/Form1.Designer.cs
+++ Pagos/Form1.Designer.cs
@@ -56,9 +56,9 @@
             this.label2.Font = new System.Drawing.Font("Gadugi", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(28, 88);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(173, 17);
+            this.label2.Size = new System.Drawing.Size(177, 17);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Digite las horas laboradas:";
+            this.label2.Text = "Digite las horas laboradas :";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
